2b02363a854709f211f186dadb59719263f695b8
BEEBI_HAJRAH MOHAMED_KHAN <beebikhan4@gmail.com>
2023-09-12 17:13:49 +0200

    Navbar responsive done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,20 @@
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
-
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Recettes from "./pages/Recettes";
+import Favoris from "./pages/Favoris";
+import "./styles/Navbar.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-
-        <Counter />
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/Homepage" element={<Homepage />} />
+        <Route path="/Recettes" element={<Recettes />} />
+        <Route path="/Favoris" element={<Favoris />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleActiveClass = () => {
+    setIsActive(!isActive);
+  };
+
+  const removeActive = () => {
+    setIsActive(false);
+  };
+
+  return (
+    <nav className="navbar">
+      <Link to="/" className="logo">
+        Free Go
+        <br />
+        <span className="slogan">Redonnez vie à votre frigo</span>
+      </Link>
+
+      <ul className={`navMenu ${isActive ? "active" : ""}`}>
+        <li onClick={removeActive}>
+          <NavLink to="/Homepage" className="navLink">
+            Homepage
+          </NavLink>
+        </li>
+        <li onClick={removeActive}>
+          <NavLink to="/Recettes" className="navLink">
+            Recettes
+          </NavLink>
+        </li>
+        <li onClick={removeActive}>
+          <NavLink to="/Favoris" className="navLink">
+            Favoris
+          </NavLink>
+        </li>
+        <li onClick={removeActive}>
+          <NavLink to="/Generateur" className="navLink">
+            <span className="navLinkText">Essayer FreeGo</span>
+          </NavLink>
+        </li>
+      </ul>
+
+      <div
+        className={`hamburger ${isActive ? "active" : ""}`}
+        onClick={toggleActiveClass}
+      >
+        <span className="bar" />
+        <span className="bar" />
+        <span className="bar" />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
