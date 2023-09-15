578601cc447ae6f6ae264f1378cd2814854b6b37
BEEBI_HAJRAH MOHAMED_KHAN <beebikhan4@gmail.com>
2023-09-15 09:15:22 +0200

    Route done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,15 @@
-import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Homepage from "./pages/Homepage";
-import Recettes from "./pages/Recettes";
-import Favoris from "./pages/Favoris";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 import "./styles/Navbar.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/Homepage" element={<Homepage />} />
-        <Route path="/Recettes" element={<Recettes />} />
-        <Route path="/Favoris" element={<Favoris />} />
-      </Routes>
+      <Content />
+      <Footer />
     </div>
   );
 }
