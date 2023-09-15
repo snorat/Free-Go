f871f86f1732523d2b202657ddd79423b96d6cca
joellekounde <djokounde@gmail.com>
2023-09-15 11:17:56 +0200

    création bouton recette OK

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { useState, useEffect } from "react";
-import axios from "axios";
 import LaSelection from "./components/LaSelection";
 import "./styles/LaSelection.css";
-import "./App.css";
 import SearchBar from "./components/SearchBar";
 import "./styles/searchBar.css";
+import Navbar from "./components/Navbar";
+import "./styles/Navbar.css";
 import "./styles/App.css";
 
 export default function App() {
-  
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    axios
-      .get("https://www.themealdb.com/api/json/v1/1/random.php")
-      .then((response) => {
-        setData(response.data.meals[0]);
-      });
-  }, []);
-  
   return (
     <div>
+      <Navbar />
       <SearchBar />
-      <div className="laSelection">
-      <LaSelection meals={data} />
-      </div>
+      <LaSelection />
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import "./App.css";
+import "./styles/App.css";
 
 const router = createBrowserRouter([
   {
