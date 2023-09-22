294bec24609efcb967918dc1ee084bda48cb2bd8
joellekounde <djokounde@gmail.com>
2023-09-22 13:35:47 +0200

    bouton de recette fonctionnel

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,23 +1,10 @@
 import React from "react";
-import SearchBar from "../components/SearchBar";
 import LaSelection from "../components/LaSelection";
-import TrendRecipe from "../components/TrendRecipe";
-import CardInsp from "../components/CardInsp";
-import Cake from "../assets/images/Cake.png";
 
 export default function Homepage() {
   return (
     <div>
-      <SearchBar />
       <LaSelection />
-      <TrendRecipe />
-      <CardInsp
-        imgSrc={Cake}
-        imgAlt="cake-image"
-        description="Si vous êtes en manque d'inspiration, pourquoi ne pas rechercher blablabla"
-        buttonText="Découvre le par toi même ici"
-        link="cardPage"
-      />
     </div>
   );
 }
